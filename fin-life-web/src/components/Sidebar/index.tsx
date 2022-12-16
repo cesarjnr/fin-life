402bf7d02c9510b35ace2db6e0a773def23ea12b
César Júnior <cesar.jnr@hotmail.com>
2022-12-16 15:37:36 -0300

    Implement the collapsed version of the menu item

--- fin-life-web/src/components/Sidebar/index.tsx
+++ fin-life-web/src/components/Sidebar/index.tsx
@@ -1,24 +1,78 @@
 import { Flex } from '@chakra-ui/react';
 import { Box } from '@chakra-ui/react';
 import { Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib';
 import { AiFillFolder } from "react-icons/ai";
+import { MdAccountBalanceWallet } from "react-icons/md";
 
 import { Logo } from '../Logo';
 
-export const Sidebar = () => (
-  <Flex
-    justify="center"
-    backgroundColor="black.800"
-    display="flex"
-    direction="column"
-    gridGap={28}
-    py={12}
->
-    <Logo />
-    <Box flex="1">
-        <Box px={8} py={4}>
-            <Icon as={AiFillFolder} w={6} h={6} />
-        </Box>
-    </Box>
-  </Flex>
-);
+interface MenuItem {
+    label: string;
+    iconComponent?: IconType;
+    subItems?: {
+        label: string;
+    }[];
+}
+
+export const Sidebar = () => {
+    const menuItems: MenuItem[] = [
+        {
+            iconComponent: AiFillFolder,
+            label: 'Gerenciamento',
+            subItems: [
+                { label: 'Categorias de Despesas' },
+                { label: 'Fluxo de Caixa' }
+            ]
+        },
+        {
+            iconComponent: MdAccountBalanceWallet,
+            label: 'Portfólio'
+        }
+    ];
+
+    return (
+        <Flex
+            justify="center"
+            backgroundColor="black.800"
+            display="flex"
+            direction="column"
+            gridGap={28}
+            py={12}
+        >
+            <Logo />
+            <Box flex="1">
+                {menuItems.map((menuItem) => (
+                    <Box
+                        key={menuItem.label}
+                        cursor="pointer"
+                        px={8}
+                        py={4}
+                        pos="relative"
+                        role="group"
+                        _hover={{
+                            backgroundColor: "rgba(0, 210, 91, 0.05)",
+                            _before: {
+                                content: `""`,
+                                backgroundColor: "green.500",
+                                height: "100%",
+                                left: 0,
+                                position: "absolute",
+                                top: 0,
+                                width: "6px"
+                            }
+                        }}
+                    >
+                        <Icon
+                            as={menuItem.iconComponent}
+                            color="white"
+                            h={6}
+                            w={6}
+                            _groupHover={{ color: 'green.500' }}
+                        />
+                    </Box>
+                ))}
+            </Box>
+        </Flex>
+    );
+};
