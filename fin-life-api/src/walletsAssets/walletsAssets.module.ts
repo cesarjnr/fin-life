27906259d91446e1710698a955fd7945794ba264
César Júnior <cesar.jnr@hotmail.com>
2023-07-14 17:31:24 -0300

    Create list wallets assets endpoint

--- fin-life-api/src/walletsAssets/walletsAssets.module.ts
+++ fin-life-api/src/walletsAssets/walletsAssets.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WalletsAssetsController } from './walletsAssets.controller';
 import { WalletsAssetsService } from './walletsAssets.service';
+import { AssetHistoricalPrice } from '../assetHistoricalPrices/assetHistoricalPrice.entity';
 import { WalletAsset } from './walletAsset.entity';
 
 @Module({
   controllers: [WalletsAssetsController],
   exports: [WalletsAssetsService],
-  imports: [TypeOrmModule.forFeature([WalletAsset])],
+  imports: [TypeOrmModule.forFeature([AssetHistoricalPrice, WalletAsset])],
   providers: [WalletsAssetsService]
 })
 export class WalletsAssetsModule {}
--- fin-life-api/src/walletsAssets/walletsAssets.service.ts
+++ fin-life-api/src/walletsAssets/walletsAssets.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { WalletAsset } from './walletAsset.entity';
+import { AssetHistoricalPrice } from '../assetHistoricalPrices/assetHistoricalPrice.entity';
 
 interface GetWalletsAssetsFilters {
   walletId?: number;
@@ -10,10 +11,32 @@ interface GetWalletsAssetsFilters {
 
 @Injectable()
 export class WalletsAssetsService {
-  constructor(@InjectRepository(WalletAsset) private readonly walletsAssetsRepository: Repository<WalletAsset>) {}
+  constructor(
+    @InjectRepository(AssetHistoricalPrice)
+    private readonly assetHistoricalPriceRepository: Repository<AssetHistoricalPrice>,
+    @InjectRepository(WalletAsset) private readonly walletsAssetsRepository: Repository<WalletAsset>
+  ) {}
 
   public async get(filters?: GetWalletsAssetsFilters): Promise<WalletAsset[]> {
-    return await this.walletsAssetsRepository.find({ where: { walletId: filters?.walletId } });
+    const subQuery = this.assetHistoricalPriceRepository
+      .createQueryBuilder('assetHistoricalPrice')
+      .distinctOn(['assetHistoricalPrice.assetId'])
+      .orderBy({
+        'assetHistoricalPrice.assetId': 'DESC',
+        'assetHistoricalPrice.date': 'DESC'
+      });
+
+    return await this.walletsAssetsRepository
+      .createQueryBuilder('walletAsset')
+      .where('walletAsset.walletId = :walletId', { walletId: filters?.walletId })
+      .leftJoinAndSelect('walletAsset.asset', 'asset')
+      .leftJoinAndSelect(
+        'asset.assetHistoricalPrices',
+        'assetHistoricalPrice',
+        `assetHistoricalPrice.id IN (${subQuery.select('id').getQuery()})`
+      )
+      .orderBy('walletAsset.assetId')
+      .getMany();
   }
 
   public async find(walletId: number, assetId: number): Promise<WalletAsset> {
