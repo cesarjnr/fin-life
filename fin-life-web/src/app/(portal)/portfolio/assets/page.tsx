a912fa228aace91ec743b67a4f11421bd07d7041
César Júnior <cesar.jnr@hotmail.com>
2023-12-29 21:11:44 -0300

    Fix asset categor and class

--- fin-life-web/src/app/(portal)/portfolio/assets/page.tsx
+++ fin-life-web/src/app/(portal)/portfolio/assets/page.tsx
@@ -1,21 +1,9 @@
 import { getUserWalletsAssets } from '@/api/wallets-assets';
-import { AssetCategories, AssetClasses } from '@/api/assets';
 import { formatCurrency } from '@/lib/currency';
 import Table, { RowData } from '@/components/table';
 
 export default async function Assets() {
   const walletsAssets = await getUserWalletsAssets(1, 1);
-  const assetsCategoriesMap = new Map<AssetCategories, string>([
-    [AssetCategories.VariableIncome, 'Renda Variável'],
-    [AssetCategories.FixedIncoe, 'Renda Fixa']
-  ]);
-  const assetsClassesMap = new Map<AssetClasses, string>([
-    [AssetClasses.Stock, 'Ação'],
-    [AssetClasses.RealState, 'Imobiliário'],
-    [AssetClasses.Cash, 'Caixa'],
-    [AssetClasses.International, 'Internacional'],
-    [AssetClasses.Cryptocurrency, 'Criptomoeda']
-  ]);
   const headers = [
     'Ticker',
     'Categoria',
@@ -32,8 +20,8 @@ export default async function Assets() {
     .map((walletAsset) => {
       const data = [
         walletAsset.asset.ticker,
-        assetsCategoriesMap.get(walletAsset.asset.category)!,
-        assetsClassesMap.get(walletAsset.asset.class)!,
+        walletAsset.asset.category,
+        walletAsset.asset.class,
         walletAsset.area || 'N/A',
         walletAsset.characteristic || 'N/A',
         walletAsset.expectedPercentage || 'N/A',
@@ -50,11 +38,12 @@ export default async function Assets() {
 
   return (
     <div className="self-center">
-      <Table
-        title="Aposentadoria"
-        headers={headers}
-        rowsData={tableRowsData}
-      />
+      <div className="p-6 rounded-xl bg-black-800">
+        <Table
+          headers={headers}
+          rowsData={tableRowsData}
+        />
+      </div>
     </div>
   );
 }
