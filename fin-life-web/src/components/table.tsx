b60351fd7014f5ff797fb403bd8b1c91225492fc
César Júnior <cesar.jnr@hotmail.com>
2023-12-29 21:12:36 -0300

    Remove title from table component

--- fin-life-web/src/components/table.tsx
+++ fin-life-web/src/components/table.tsx
@@ -1,57 +1,42 @@
 export interface TableProps {
   headers: string[];
   rowsData: RowData[];
-  title?: string;
 }
 export interface RowData {
   id: string | number;
   values: (string | number)[];
 }
 
-export default function Table({
-  headers,
-  rowsData,
-  title
-}: TableProps) {
+export default function Table({ headers, rowsData }: TableProps) {
   return (
-    <div className="py-4 px-6 bg-black-800 rounded-xl flex flex-col gap-8">
-      <div className="flex justify-between">
-        {
-          title && 
-          <h2 className="text-2xl font-semibold">
-            {title}
-          </h2>
-        }
-      </div>
-      <table className="table-auto">
-        <thead>
-          <tr className="border-b border-white/[.1] text-sm text-left">
-            {headers.map((header) => (
-              <th
-                key={header}
-                className="p-4"
-              >{header}</th>
+    <table className="table-auto w-full bg-black-800">
+      <thead>
+        <tr className="border-b border-white/[.1] text-sm text-left">
+          {headers.map((header) => (
+            <th
+              key={header}
+              className="p-4 text-center"
+            >{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rowsData.map((rowData) => (
+          <tr
+            key={rowData.id}
+            className="border-b border-white/[.1] text-sm text-white/[.6]"
+          >
+            {rowData.values.map((value, index) => (
+              <td
+                key={`${rowData.id}-${index}-${value}`}
+                className="p-4 text-center"
+              >
+                {value}
+              </td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {rowsData.map((rowData) => (
-            <tr
-              key={rowData.id}
-              className="border-b border-white/[.1] text-sm text-white/[.6]"
-            >
-              {rowData.values.map((value, index) => (
-                <td
-                  key={`${rowData.id}-${index}-${value}`}
-                  className="p-4"
-                >
-                  {value}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 }
