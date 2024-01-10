6e07e8e6865f77b53dff5c7a27ec6243c5989be7
César Júnior <cesar.jnr@hotmail.com>
2024-01-10 15:43:29 -0300

    Refactor table component

--- fin-life-web/src/components/table/index.tsx
+++ fin-life-web/src/components/table/index.tsx
@@ -11,40 +11,49 @@ export interface RowData {
 
 export default function Table({ headers, rowsData }: TableProps) {
   return (
-    <table className="table-auto w-full bg-black-800">
-      <thead>
-        <tr className="border-b border-white/[.1] text-sm text-left">
-          {headers.map((header) => (
-            <th
-              key={header}
-              className="p-4 text-center"
-            >{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rowsData.map((rowData) => (
-          <tr
-            key={rowData.id}
-            className={`
-              border-b
-              border-white/[.1]
-              text-sm
-              text-white/[.6]
-              ${rowData.onClick ? 'cursor-pointer hover:bg-white/[.02]' : ''}
-            `}
-          >
-            {rowData.values.map((value, index) => (
-              <td
-                key={`${rowData.id}-${index}-${value}`}
-                className="p-4 text-center"
-              >
-                {value}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className="self-start">
+      {rowsData.length ?
+        (
+          <table className="table-auto w-full bg-black-800">
+            <thead>
+              <tr className="border-b border-white/[.1] text-sm text-left">
+                {headers.map((header) => (
+                  <th
+                    key={header}
+                    className="p-4 text-center"
+                  >{header}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {rowsData.map((rowData) => (
+                <tr
+                  key={rowData.id}
+                  className={`
+                    border-b
+                    border-white/[.1]
+                    text-sm
+                    text-white/[.6]
+                    ${rowData.onClick ? 'cursor-pointer hover:bg-white/[.02]' : ''}
+                  `}
+                >
+                  {rowData.values.map((value, index) => (
+                    <td
+                      key={`${rowData.id}-${index}-${value}`}
+                      className="p-4 text-center"
+                    >
+                      {value}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) :
+        (
+          <div className="p-4">No data</div>
+        )
+      }
+    </div>
   );
 }
