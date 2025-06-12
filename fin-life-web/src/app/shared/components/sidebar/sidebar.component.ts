6190c0a35bbac55fe02a47900372d044e4ce0792
César Júnior <cesar.jnr@hotmail.com>
2025-06-12 12:56:59 +0100

    Add subitems config to sidebar

--- fin-life-web/src/app/shared/components/sidebar/sidebar.component.ts
+++ fin-life-web/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  ActivationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { debounceTime, filter } from 'rxjs';
 
 interface SidebarNavItem {
   label: string;
-  link: string;
+  navigateTo?: string;
+  route?: string;
+  subItems?: SidebarNavItem[];
 }
 
 @Component({
@@ -13,10 +21,85 @@ interface SidebarNavItem {
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-  public items: SidebarNavItem[] = [
-    { label: 'Produtos', link: 'admin/products' },
-    { label: 'Ativos', link: 'portfolios/1/assets' },
-    { label: 'Operações', link: 'portfolios/1/buys-sells' },
+export class SidebarComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  public readonly selectedItem = signal<SidebarNavItem | undefined>(undefined);
+  public readonly items: SidebarNavItem[] = [
+    {
+      label: 'Admin',
+      route: 'admin',
+      subItems: [{ label: 'Produtos', navigateTo: 'admin/products' }],
+    },
+    {
+      label: 'Portfolio',
+      route: 'portfolios',
+      subItems: [
+        { label: 'Ativos', navigateTo: 'portfolios/:portfolioId/assets' },
+        {
+          label: 'Operações',
+          navigateTo: 'portfolios/:portfolioId/buys-sells',
+        },
+      ],
+    },
   ];
+
+  public ngOnInit(): void {
+    this.setupNavConfigs();
+  }
+
+  public handleItemClick(item: SidebarNavItem): void {
+    if (item.label !== this.selectedItem()?.label) {
+      this.selectedItem.set(item);
+    } else {
+      this.selectedItem.set(undefined);
+    }
+  }
+
+  private setupNavConfigs(): void {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof ActivationEnd),
+        debounceTime(50),
+      )
+      .subscribe({
+        next: (event) => {
+          const currentRouteParamsEntries = Object.entries(
+            event.snapshot.params,
+          );
+
+          this.replaceItemRouteParamsConfig(
+            currentRouteParamsEntries,
+            this.items,
+          );
+          this.setInitialSelectedItem();
+        },
+      });
+  }
+
+  private replaceItemRouteParamsConfig(
+    currentRouteParamsEntries: [string, any][],
+    items?: SidebarNavItem[],
+  ): void {
+    if (items?.length) {
+      items.forEach((item) => {
+        currentRouteParamsEntries.forEach(([key, value]) => {
+          item.navigateTo =
+            item.navigateTo?.replace(`:${key}`, value) || item.navigateTo;
+        });
+
+        this.replaceItemRouteParamsConfig(
+          currentRouteParamsEntries,
+          item.subItems,
+        );
+      });
+    }
+  }
+
+  private setInitialSelectedItem(): void {
+    const baseRoute = this.router.url.split('/')[1];
+    const currentItem = this.items.find((item) => item.route === baseRoute);
+
+    this.selectedItem.set(currentItem);
+  }
 }
