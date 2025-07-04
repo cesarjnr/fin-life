5ea8c578889e1ed7f0e9c4e076d022248f45adf5
César Júnior <cesar.jnr@hotmail.com>
2025-07-03 21:50:23 -0300

    Fix cors settings

--- fin-life-api/src/main.ts
+++ fin-life-api/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN,
+      credentials: true
+    }
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
