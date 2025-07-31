9a8aba819fe6700cc03a52d5c5adfdb0d3aa621f
César Júnior <cesar.jnr@hotmail.com>
2025-07-31 20:30:32 -0300

    Change material date input adapter

--- fin-life-web/src/app/app.config.ts
+++ fin-life-web/src/app/app.config.ts
@@ -6,7 +6,12 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import {
+  MAT_DATE_FNS_FORMATS,
+  provideDateFnsAdapter,
+} from '@angular/material-date-fns-adapter';
+import { enUS } from 'date-fns/locale';
 import { provideToastr } from 'ngx-toastr';
 import { provideNgxMask } from 'ngx-mask';
 
@@ -23,18 +28,20 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([authInterceptor, errorHandlingInterceptor]),
     ),
     provideAnimationsAsync(),
-    provideNativeDateAdapter({
+    provideDateFnsAdapter({
       parse: {
-        dateInput: 'LL',
+        dateInput: 'yyyy-MM-dd',
       },
       display: {
-        dateInput: 'DD/MM/YYYY',
-        monthYearLabel: 'MMM YYYY',
-        dateA11yLabel: 'LL',
-        monthYearA11yLabel: 'MMMM YYYY',
+        dateInput: 'yyyy-MM-dd',
+        monthYearLabel: 'MMM yyyy',
+        dateA11yLabel: 'PPPP',
+        monthYearA11yLabel: 'MMMM yyyy',
       },
     }),
     provideToastr({ positionClass: 'toast-bottom-center' }),
     provideNgxMask(),
+    { provide: MAT_DATE_LOCALE, useValue: enUS },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_DATE_FNS_FORMATS },
   ],
 };
