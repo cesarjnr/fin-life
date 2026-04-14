ee92e157fbfef397f9fcd36e84057637d57dad53
César Júnior <cesar.jnr@hotmail.com>
2026-04-14 18:17:30 -0300

    Add name field to products modal

--- fin-life-web/src/app/features/admin/products/product-modal/product-modal.component.ts
+++ fin-life-web/src/app/features/admin/products/product-modal/product-modal.component.ts
@@ -31,6 +31,7 @@ import { CommonService } from '../../../../core/services/common.service';
 import { Currencies } from '../../../../core/dtos/common.dto';
 
 interface ProductForm {
+  name: FormControl<string | null>;
   code: FormControl<string | null>;
   category: FormControl<string | null>;
   assetClass: FormControl<string | null>;
@@ -67,6 +68,7 @@ export class ProductModalComponent {
   public readonly cancelModal = output<void>();
   public readonly saveProduct = output<Asset>();
   public readonly productForm = this.formBuilder.group<ProductForm>({
+    name: this.formBuilder.control('', Validators.required),
     code: this.formBuilder.control('', Validators.required),
     category: this.formBuilder.control('', Validators.required),
     assetClass: this.formBuilder.control('', Validators.required),
@@ -108,6 +110,7 @@ export class ProductModalComponent {
       if (asset) {
         this.productForm.addControl('active', this.formBuilder.control(false));
         this.productForm.setValue({
+          name: asset.name,
           code: asset.code,
           category: asset.category,
           assetClass: asset.class,
